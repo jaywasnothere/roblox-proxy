75d94cd87491041fe83c32cdecbc43b7ca777e5c
jaywasnothere <cherrybolt997@gmail.com>
2025-07-18 13:47:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ app.use(express.json());
 const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1395760129328283650/WUXgotpYa37wYv42RCZu5-Ef8kMm3i4fLQ8Y01lrVbaengu1HUC9yFr96Asy2uP7mxKz";
 
 app.post('/send-log', async (req, res) => {
-  const { username, userId } = req.body;
+  const { players } = req.body;
+  
   if (!username || !userId) return res.status(400).send("Missing data");
 
+  const user1 = players[0].username
+  const user2 = players[1].username
+    
   try {
     await axios.post(DISCORD_WEBHOOK, {
-      content: `👤 **${username}** joined (UserId: ${userId})`
+      content: `@everyone ${user1} Has completed a trade with ${user2}`
     });
     res.status(200).send("Sent to Discord.");
   } catch (e) {
